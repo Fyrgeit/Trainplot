68f4a22791b1f8b16f538af3be4d0f8db6c9fd97
Fyrgeit <mans@peronilla.se>
2024-05-21 21:31:15 +0200

    plot?

--- index.ts
+++ index.ts
@@ -1,23 +1,40 @@
 import { plot } from 'nodeplotlib';
-import type { Plot } from 'nodeplotlib'
+import type { Plot } from 'nodeplotlib';
 
 const xs: number[] = [];
 const ys: number[] = [];
 
-for (let t = 0; t < 10; t++) {
-    xs.push(t)
-    ys.push(t**2)    
+let d = 0;
+let v = 40;
+let t = 0;
+let timer = 0;
+
+while (t < 3600) {
+    if (t > 10 && v > 10) {
+        v -= 0.5;
+    } else if (t > 120 && v > 0) {
+        v -= 0.4;
+    }
+
+    d += v;
+
+    xs.push(d);
+    ys.push(-t);
+    t++;
+    timer++;
+
+    if (t >= 200) break;
 }
 
 const data: Plot[] = [
-  {
-    x: xs,
-    y: ys,
-    type: 'scatter',
-    line: {
-        color: 'red'
-    }
-  },
+    {
+        x: xs,
+        y: ys,
+        type: 'scatter',
+        line: {
+            color: 'red',
+        },
+    },
 ];
 
 plot(data);
