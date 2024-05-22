c10f7f9ff81c4bd59b6ea614398e1c0ada018d00
Fyrgeit <mans@peronilla.se>
2024-05-22 20:07:57 +0200

    steps

--- index.ts
+++ index.ts
@@ -1,30 +1,70 @@
 import { plot } from 'nodeplotlib';
 import type { Plot } from 'nodeplotlib';
 
+const acc = 1;
+const dec = 0.75;
+let d = 0;
+let v = 80;
+
+const steps: `${'wait' | 'slowTo' | 'accTo'}: ${number}`[] = [
+    'wait: 5',
+    'slowTo: 40',
+    'wait: 5',
+    'slowTo: 10',
+    'wait: 5',
+    'slowTo: 0',
+    'accTo: 40',
+    'wait: 15',
+    'slowTo: 0',
+];
+
 const xs: number[] = [];
 const ys: number[] = [];
 
-let d = 0;
-let v = 40;
-let t = 0;
-let timer = 0;
+xs.push(d);
+ys.push(v);
 
-while (t < 3600) {
-    if (t > 10 && v > 10) {
-        v -= 0.5;
-    } else if (t > 120 && v > 0) {
-        v -= 0.4;
+steps.forEach((step) => {
+    const type = (step as unknown as string).split(': ')[0] as
+        | 'wait'
+        | 'slowTo'
+        | 'accTo';
+    const value = +(step as unknown as string).split(': ')[1];
+
+    if (type === 'wait') {
+        d += v * value;
+
+        xs.push(d);
+        ys.push(v);
+    } else if (type === 'slowTo') {
+        while (v > value) {
+            v -= dec;
+            d += v;
+
+            if (v - value < dec) {
+                v = value;
+            }
+
+            xs.push(d);
+            ys.push(v);
+        }
+    } else if (type === 'accTo') {
+        while (v < value) {
+            v += acc;
+            d += v;
+
+            if (value - v < acc) {
+                v = value;
+            }
+
+            xs.push(d);
+            ys.push(v);
+        }
     }
+});
 
-    d += v;
-
-    xs.push(d);
-    ys.push(-t);
-    t++;
-    timer++;
-
-    if (t >= 200) break;
-}
+xs.push(d);
+ys.push(v);
 
 const data: Plot[] = [
     {
